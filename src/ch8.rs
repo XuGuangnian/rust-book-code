e65c803dba36c1010a38a286a95ca504d3c61005
XuGuangnian <gengen.wang@outlook.com>
2022-07-09 00:26:34 +0800

    refactor ch8 code

--- src/ch8.rs
+++ src/ch8.rs
@@ -2,3 +2,57 @@ pub mod string;
 pub mod vector;
 pub mod hashmap;
 pub mod exercise;
+
+pub fn exercises() {
+    exercise::one();
+
+    exercise::two();
+
+    exercise::three();
+}
+
+pub(crate) fn hashmap() {
+    hashmap::create();
+
+    hashmap::ownership();
+
+    hashmap::access_values();
+
+    hashmap::iterate();
+
+    hashmap::update();
+}
+
+pub(crate) fn string() {
+    string::create();
+
+    string::update();
+
+    string::combine_two_strings();
+
+    string::combine_multiple_strings();
+
+    string::not_indexed();
+
+    string::slice();
+
+    string::iterate();
+}
+
+pub(crate) fn vector() {
+    vector::create();
+
+    vector::update();
+
+    vector::drop();
+
+    vector::read_element();
+
+    vector::vector_work_type();
+
+    vector::iterate();
+
+    vector::iterate_mut();
+
+    vector::store_multiple_types_by_enum();
+}
--- src/main.rs
+++ src/main.rs
@@ -8,65 +8,17 @@ use crate::ch8::hashmap;
 use crate::ch8::exercise;
 
 fn main() {
-    // ch8_vector();
-
-    // ch8_string();
-
-    // ch8_hashmap();
-
-    ch8_exercises();
+    ch8();
 }
 
-fn ch8_exercises() {
-    exercise::one();
+pub fn ch8() {
+    ch8::vector();
 
-    exercise::two();
+    ch8::string();
 
-    exercise::three();
+    ch8::hashmap();
+
+    ch8::exercises();
 }
 
-fn ch8_hashmap() {
-    hashmap::create();
 
-    hashmap::ownership();
-
-    hashmap::access_values();
-
-    hashmap::iterate();
-
-    hashmap::update();
-}
-
-fn ch8_string() {
-    string::create();
-
-    string::update();
-
-    string::combine_two_strings();
-
-    string::combine_multiple_strings();
-
-    string::not_indexed();
-
-    string::slice();
-
-    string::iterate();
-}
-
-fn ch8_vector() {
-    vector::create();
-
-    vector::update();
-
-    vector::drop();
-
-    vector::read_element();
-
-    vector::vector_work_type();
-
-    vector::iterate();
-
-    vector::iterate_mut();
-
-    vector::store_multiple_types_by_enum();
-}
